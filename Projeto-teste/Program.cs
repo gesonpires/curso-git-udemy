f22a0335c611815e18542c2b27787afd1244ca5a
Geson Pires <geson.pires@gmail.com>
2020-04-03 20:26:30 -0300

    Projeto iniciado, monstrando a mensagem FLAMENGO CAMPEÃO DO MUNDO 2020

---
+++ Projeto-teste/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FLAMENGO CAMPEÃO DO MUNDO 2020!");
+            Console.ReadLine();
+        }
+    }
+}
