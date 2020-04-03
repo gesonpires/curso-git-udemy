c5c287917bdfbaa87169cc58e424918d81c6192e
Geson Pires <geson.pires@gmail.com>
2020-04-03 20:39:17 -0300

    Acrescentada uma pergunta.

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -8,6 +8,7 @@ namespace Projeto_teste
         {
             Console.WriteLine("FLAMENGO CAMPEÃO DO MUNDO 2020!");
             Console.WriteLine("AVAÍ NÃO SERÁ CAMPEÃO DO MUNDO 2020!");
+            Console.WriteLine("COMO VOCÊ ESTÁ?");
             Console.ReadLine();
         }
     }
